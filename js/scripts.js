2a43c0b45ec0eaf628be29952d6877103b685c50
mattsperez <131983739+mattsperez@users.noreply.github.com>
2023-07-11 23:01:15 -0500

    update scripts
    added pokemonList, three objects with mixed arrays.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello world');
+var pokemonList;
+let pokemonList = [
+    'Squirtle',
+    0.5,
+    [grass]
+];
+let pokemonList = [
+    'Vulpix',
+    0.6,
+    [fire]
+];
+let pokemonList = [
+    'Scyther',
+    1.5,
+    ['bug', 'flying']
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+];
