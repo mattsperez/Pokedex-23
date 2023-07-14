6e88b28f8bea4ee53617322255a58768d30bc0d6
mattsperez <131983739+mattsperez@users.noreply.github.com>
2023-07-13 20:57:43 -0500

    updated syntax, loops, conditionals
    changed var to let.
    added a pokemon and changed one out.
    added a for loop and conditionals.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,37 @@
-var pokemonList = [
+let pokemonList = [
     
     {
         name: "Squirtle",
-        height: 0.5,
-        types: ['water']
+        height: "0.5",
+        type: ["water"]
     },
     {
         name: "Vulpix",
-        height: 0.6,
-        types: ['fire']
+        height: "0.6",
+        type: ["fire"]
     },
     {
-        name: "Scyther",
-        height: 1.5,
-        types: ['bug', 'flying']
+        name: "Arbok",
+        height: "3.5",
+        type: ["poison"]
+    },
+    {
+        name: "Cubone",
+        height: "0.4",
+        type: ["ground"]
     }
 
 ];
+for (i = 0; i < pokemonList.length; i++) {
+    console.log(pokemonList[i]);
+    document.write(pokemonList[i].name + " ( height: " + pokemonList[i].height + " m ) ");
+//this just gives displays all pokemon and height included 
+    if(i >= 1.0) {
+        console.log(" - aww cute\!");
+        document.write(" - aww cute\! ");
+    } else if (i <= 1.1) {
+        console.log (" - damn, that\'s huge\!");
+        document.write(" - damn, that\'s huge\! ");
+    };
+//displays a message based on the height of each pokemon
+};
