10b38bac2838c24ffb2eddc2b67594101e5bb59d
mattsperez <131983739+mattsperez@users.noreply.github.com>
2023-07-19 11:48:23 -0500

    forEach
    sytanx updated to forEach in place of a for Loop

--- js/scripts.js
+++ js/scripts.js
@@ -22,16 +22,7 @@ let pokemonList = [
     }
 
 ];
-for (i = 0; i < pokemonList.length; i++) {
-    console.log(pokemonList[i]);
-    document.write("<p>" + pokemonList[i].name + " ( height: " + pokemonList[i].height + " m ) ");
-//this just gives displays all pokemon and height included 
-    if(i >= 1.0) {
-        console.log(" - aww cute\!");
-        document.write(" - aww cute\! ");
-    } else if (i <= 1.1) {
-        console.log (" - damn, that\'s huge\!");
-        document.write(" - damn, that\'s huge\! ");
-    };
-//displays a message based on the height of each pokemon
-};
+pokemonList.forEach(function(pokemon) {
+    console.log(pokemon.name + " ( height: " + pokemon.height + " m ) ");
+   document.write(pokemon.name + " ( height: " + pokemon.height + " m ) </br> ");
+});
