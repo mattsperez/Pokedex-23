7e69471003b301d6f891a48d58e0e299bc538423
mattsperez <131983739+mattsperez@users.noreply.github.com>
2023-07-27 21:29:26 -0500

    modal
    created modals
    created close buttons and event listeners
    updated the showDetails

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ let pokemonRepository = (function () {
 let pokemonList = [];
 
 let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+let modalContainer = document.querySelector("#modal-container");
+let container = document.querySelector("#image-container");
 
     //function to add pokemon and to reject if a non pokemon is added
     function add (pokemon){
@@ -60,11 +62,64 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
         });
     }
 
-    function showDetails(item) {
-        pokemonRepository.loadDetails(item).then(function () {
-            console.log(item);
-        });
+        function showModal(pokemon) {
+      
+            modalContainer.innerHTMl = "";
+              
+            let modal = document.createElement("div");
+              modal.classList.add("modal");
         
+            let closeButtonElement = document.createElement("button");
+              closeButtonElement.classList.add("modal-close");
+              closeButtonElement.innerText = "Close";
+              closeButtonElement.addEventListener("click", hideModal);
+        
+            let nameElement = document.createElement("h1");
+              nameElement.innerText = pokemon.name;
+        
+            let heightElement = document.createElement("p");
+              heightElement.innerText = "height: " + " " + pokemon.height;
+        
+            let typesElement = document.createElement("p");
+              typesElement.innerText = "types: " + " " + pokemon.types;
+        
+            let imageElement = document.createElement("img");
+              imageElement.setAttribute("src", pokemon.imageUrl);
+        
+            modal.append(closeButtonElement);
+            modal.append(nameElement);
+            modal.append(heightElement);
+            modal.append(typesElement);
+            modal.append(imageElement);
+            modalContainer.appendChild(modal);
+        
+            modalContainer.classList.add("is-visible");
+          }
+        
+          function hideModal(){
+            let modalContainer = document.querySelector("#modal-container");
+            modalContainer.classList.remove("is-visible")
+          }
+        
+          window.addEventListener("keydown", (e) => {
+            let modalContainer = document.querySelector("#modal-container");
+            if (e.key === "escape" && modalContainer.classList.contains("is-visible")){
+              hideModal();
+            }
+          });
+
+          modalContainer.addEventListener("click", (e) => {
+            let target = e.target;
+            if (target === modalContainer) {
+                hideModal();
+            }
+          });
+
+          function showDetails(pokemon) {
+            loadDetails(pokemon).then(function () {
+            showModal(pokemon);
+            });      
+
     }
 
     return {
@@ -73,12 +128,11 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
         addListItem: addListItem,
         loadList: loadList,
         loadDetails: loadDetails,
-        showDetails: showDetails
+        showDetails: showDetails,
+        showModal: showModal,
+        hideModal: hideModal
     };
 })();
-
-    //added a pokemon
-    pokemonRepository.add({name: "Dratini", height: "1.8", type: ["dragon"]});
 
     pokemonRepository.loadList().then(function() {
         pokemonRepository.getAll().forEach(function(pokemon){
